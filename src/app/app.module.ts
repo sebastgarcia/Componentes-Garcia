626ad00c86074e28bd4f6536b5155f247aa08ffa
sebastgarcia <sebi_17@hotmail.com>
2023-03-22 09:24:17 -0300

    se crearon componentes con estilos de boostrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
+import { PageWrapperComponent } from './page-wrapper/page-wrapper.component';
+
+//ESTOS DE ARRIBA ES LO QUE SE FUE CREANDO, LAS CARPETAS A LAS QUE HACE REFERENCIA
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    SidebarComponent,
+    HomeComponent,
+    PageWrapperComponent
+
+//ESTE ARRAY DE ARRIBA TIENE TODOS LOS COMPIOENENTES.
   ],
   imports: [
     BrowserModule
